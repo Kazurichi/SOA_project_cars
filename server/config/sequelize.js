b44e7094b5c1c036abd9c7333b80f170842d5327
Kevin Jonathan <kevinjonathan@istts.onmicrosoft.com>
2023-05-04 11:18:29 +0700

    project initialization (1)

---
+++ server/config/sequelize.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+const db = new Sequelize(
+  'db_car_report', // DB_NAME
+  'root', // DB_USER
+  '', // DB_PASSWORD
+  {
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: "mysql",
+    logging: console.log,
+    timezone: "+07:00",
+  }
+);
+
+
+module.exports = {
+  initDB: () => {
+    return db.authenticate();
+  },
+  getDB: () => {
+    return db;
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-//test
+const express = require("express")
+const app = express()
+app.set("port", 3000)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+app.listen(app.get("port"), () => {
+    console.log(`Server started at http://localhost:${app.get("port")}`)
+})
+
+module.exports = app
