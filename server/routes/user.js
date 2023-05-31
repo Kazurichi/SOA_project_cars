def574f161ba420b79d69c2490d1bc31f3953686
FelixFn <83489247+FelixFNFN@users.noreply.github.com>
2023-05-31 23:00:48 +0700

    Penambahan Command 'npm start' + bagian yang mau aku kerjain
    tambah command 'npm start' biar lebih gampang start e + bagian yang mau tak kerjain aku comment atasnya

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+//Felix
 router.post("/register",async(req,res)=>{
 
-})
+});
+
+//Felix
 router.post("/login",async(req,res)=>{
     
-})
+});
+
+//Felix
 router.get("/tiers",async(req,res)=>{
     
-})
+});
+
+//Felix
 router.post("/subsrciption",async(req,res)=>{
     
-})
+});
 
 module.exports = router
