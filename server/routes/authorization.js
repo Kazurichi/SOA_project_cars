4cb9b91ca993838f2d0eca6f3c7b117b0b6327f9
FelixFn <83489247+FelixFNFN@users.noreply.github.com>
2023-06-16 22:50:38 +0700

    Authorization

--- server/routes/authorization.js
+++ server/routes/authorization.js
@@ -1,13 +1,81 @@
-const express = require('express')
+const express = require('express');
+const Reporter = require('../models/Reporter');
+const Manufacturer = require('../models/Manufacturer');
 const router = express.Router()
 
 //authorize reporter
+//Felix
 router.put("/reporter",async(req,res)=>{
+    let {id} = req.params;
+    let reporter = await Reporter.findOne({
+        where:{
+            idx:id
+        }
+    });
 
-})
+    if(reporter){
+        if(!reporter.authorized){
+            let updateReporter= await Reporter.update({
+                authorized:1
+            },{
+                where:{
+                    idx:id
+                }
+            });
+
+            return res.status(200).send({
+                reporter:reporter.name,
+                message:"Given Authorization"
+            });
+        }
+        return res.status(200).send({
+            reporter:reporter.name,
+            message:"Already have Authorization"
+        });
+    }
+    else{
+        return res.status(404).send({
+            message:"Not Found"
+        })
+    }
+
+
+});
 //authorize manufacter
+//Felix
 router.put("/manufacturer",async(req,res)=>{
-    
-})
+    let {id} = req.params;
+    let manufacter = await Manufacturer.findOne({
+        where:{
+            idx:id
+        }
+    });
 
-module.exports = router
+    if(manufacter){
+        if(!manufacter.authorized){
+            let updateManufacturer= await Manufacturer.update({
+                authorized:1
+            },{
+                where:{
+                    idx:id
+                }
+            });
+
+            return res.status(200).send({
+                manufacter:manufacter.name,
+                message:"Given Authorization"
+            });
+        }
+        return res.status(200).send({
+            manufacter:manufacter.name,
+            message:"Already have Authorization"
+        });
+    }
+    else{
+        return res.status(404).send({
+            message:"Not Found"
+        })
+    }
+});
+
+module.exports = router;
